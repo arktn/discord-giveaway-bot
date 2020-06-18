a4d234cb9af86f39f6b18f0b58498d72e3be198a
Maxi <64563726+notmaxi@users.noreply.github.com>
2020-06-18 21:13:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { prefix, token } = require('./config.json');
 client.on('ready', () => {
   console.log(`Bot tag: ${client.user.tag}`);
   console.log(`Guilds: ${client.guilds.cache.size}`);
-  client.user.setActivity(`with ${prefix}giveaway & ${prefix}help`, { type: 'PLAYING' });
+  client.user.setActivity(`${prefix}giveaway & ${prefix}help`, { type: 'PLAYING' });
 });
 
 client.on('message', async message => {
@@ -60,11 +60,11 @@ client.on('message', async message => {
   message.channel.send('Check your DMs!');
   message.delete();
   const embed = new Discord.MessageEmbed()
-  .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/notmaxi/discord-giveaway-bot')
+  .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/notmaxi/giveaway-bot')
   .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
   .setTitle('Help')
   .setColor('7289da')
-  .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/notmaxi/discord-giveaway-bot)`)
+  .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/notmaxi/giveaway-bot)`)
   .addField(`${prefix}giveaway [duration] [prize]`, 'Duration is statet in hours so it has to be a number and atleast 1.\nPrize can be anything but it has to be atleast one symbol.')
   .addField('Example:', `${prefix}giveaway 10 $9.99 Nitro\nCreates a 10 hour long giveaway with '$9.99 Nitro' as prize.`)
   .setFooter('Made with 💖 and discord.js by notmaxi', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
@@ -72,11 +72,11 @@ client.on('message', async message => {
   }
   if (!message.guild) {
     const embed = new Discord.MessageEmbed()
-  .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/notmaxi/discord-giveaway-bot')
+  .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/notmaxi/giveaway-bot')
   .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
   .setTitle('Help')
   .setColor('7289da')
-  .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/notmaxi/discord-giveaway-bot)`)
+  .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/notmaxi/giveaway-bot)`)
   .addField(`${prefix}giveaway [duration] [prize]`, 'Duration is statet in hours so it has to be a number and atleast 1.\nPrize can be anything but it has to be atleast one symbol.')
   .addField('Example:', `${prefix}giveaway 10 $9.99 Nitro\nCreates a 10 hour long giveaway with '$9.99 Nitro' as prize.`)
   .setFooter('Made with 💖 and discord.js by notmaxi', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
