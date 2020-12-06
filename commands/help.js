fb2dab6b83d850f609814afc9c21cc0e7f8eca0a
fekt <64563726+fekt@users.noreply.github.com>
2020-12-06 13:51:25 +0100

    💬 ✏️ Added text explaining the time variables, updated text
    💬 Added new text explaining the new time variables that have been added.
    ✏️ Updated old text and links.

--- commands/help.js
+++ commands/help.js
@@ -9,26 +9,26 @@ module.exports = {
             message.channel.send('Check your DMs!');
             message.delete();
             let embed = new Discord.MessageEmbed()
-            .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/notmaxi/discord-giveaway-bot')
+            .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/fekt/discord-giveaway-bot')
             .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
             .setTitle('Help')
             .setColor('7289da')
-            .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/notmaxi/discord-giveaway-bot)`)
+            .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/fekt/discord-giveaway-bot)`)
             .addField(`${prefix}giveaway [duration] [prize]`, 'Duration is statet in hours so it has to be a number and atleast 1.\nPrize can be anything but it has to be atleast one symbol.')
             .addField('Example:', `${prefix}giveaway 10 $9.99 Nitro\nCreates a 10 hour long giveaway with '$9.99 Nitro' as prize.`)
-            .setFooter('Made with 💖 and discord.js by notmaxi', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setFooter('Made with 💖 and discord.js by fekt', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
             message.author.send(embed);
             }
         if (!message.guild) {
             let embed = new Discord.MessageEmbed()
-            .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/notmaxi/discord-giveaway-bot')
+            .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/fekt/discord-giveaway-bot')
             .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
             .setTitle('Help')
             .setColor('7289da')
-            .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/notmaxi/discord-giveaway-bot)`)
+            .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/fekt/discord-giveaway-bot)`)
             .addField(`${prefix}giveaway [duration] [prize]`, 'Duration is statet in hours so it has to be a number and atleast 1.\nPrize can be anything but it has to be atleast one symbol.')
-            .addField('Example:', `${prefix}giveaway 10 $9.99 Nitro\nCreates a 10 hour long giveaway with '$9.99 Nitro' as prize.`)
-            .setFooter('Made with 💖 and discord.js by notmaxi', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .addField('Example:', `${prefix}giveaway 10h $9.99 Nitro\nCreates a 10 hour long giveaway with '$9.99 Nitro' as prize.`)
+            .setFooter('Made with 💖 and discord.js by fekt', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
             message.author.send(embed);
         }
     }
