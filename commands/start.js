491d77e84d6f4b0a11f815d3faf9d2ea66173403
haku <fektbox@gmail.com>
2021-04-23 00:03:08 +0200

    💬 Refurbished messages and embed
    💬 Refurbished error messages.
    💬 Refurbished giveaway messages.
    💬 Refurbished winner message.
    💬 Refurbished giveaway embed.

--- commands/start.js
+++ commands/start.js
@@ -3,27 +3,27 @@ const ms = require('ms');
 exports.run = async (client, message, args) => {
 
     if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
-        return message.channel.send(':x: You need to have the manage messages permissions to start giveaways.');
+        return message.channel.send(':boom: You need to have the manage messages permissions to start giveaways.');
     }
 
     let giveawayChannel = message.mentions.channels.first();
     if(!giveawayChannel){
-        return message.channel.send(':x: You have to mention a valid channel!');
+        return message.channel.send(':boom: Uh oh, I couldn\'t find that channel! Try again!');
     }
 
     let giveawayDuration = args[1];
     if(!giveawayDuration || isNaN(ms(giveawayDuration))){
-        return message.channel.send(':x: You have to specify a valid duration!');
+        return message.channel.send(':boom: Hm. I can\'t seem to get a number from that. Can you try again?');
     }
 
     let giveawayNumberWinners = args[2];
     if(isNaN(giveawayNumberWinners) || (parseInt(giveawayNumberWinners) <= 0)){
-        return message.channel.send(':x: You have to specify a valid number of winners!');
+        return message.channel.send(':boom: Uh... that doesn\'t look like a valid number.');
     }
 
     let giveawayPrize = args.slice(3).join(' ');
     if(!giveawayPrize){
-        return message.channel.send(':x: You have to specify a valid prize!');
+        return message.channel.send(':boom: Oh, it seems like you didn\'t give me a valid prize!');
     }
 
     client.giveawaysManager.start(giveawayChannel, {
@@ -32,13 +32,13 @@ exports.run = async (client, message, args) => {
         winnerCount: giveawayNumberWinners,
         hostedBy: client.config.hostedBy ? message.author : null,
         messages: {
-            giveaway: (client.config.everyoneMention ? "@everyone\n\n" : "")+"🎉🎉 **GIVEAWAY** 🎉🎉",
-            giveawayEnded: (client.config.everyoneMention ? "@everyone\n\n" : "")+"🎉🎉 **GIVEAWAY ENDED** 🎉🎉",
+            giveaway: (client.config.everyoneMention ? "@everyone\n\n" : "")+":tada: **GIVEAWAY** :tada:",
+            giveawayEnded: (client.config.everyoneMention ? "@everyone\n\n" : "")+":tada: **GIVEAWAY ENDED** :tada:",
             timeRemaining: "Time remaining: **{duration}**!",
             inviteToParticipate: "React with 🎉 to participate!",
-            winMessage: "Congratulations, {winners}! You won **{prize}**!",
+            winMessage: "Congratulations, {winners}! You won the **{prize}**!",
             embedFooter: "Giveaways",
-            noWinner: "Giveaway cancelled, no valid participations.",
+            noWinner: "Not enough entrants to determine a winner!",
             hostedBy: "Hosted by: {user}",
             winners: "winner(s)",
             endedAt: "Ended at",
@@ -52,6 +52,6 @@ exports.run = async (client, message, args) => {
         }
     });
 
-    message.channel.send(`Giveaway started in ${giveawayChannel}!`);
+    message.channel.send(`:tada: Done! The giveaway for the \`${prize}\` is starting in ${giveawayChannel}!`);
 
 };
