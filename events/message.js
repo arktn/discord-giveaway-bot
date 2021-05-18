a1778f602a347e461c8ccc5516d96e4395efd9d3
Tanya <80748971+Tanya575@users.noreply.github.com>
2021-05-18 21:48:23 +0400

    update message.js
    so i add something in this file like check methods so the bot has less crashs

--- events/message.js
+++ events/message.js
@@ -1,14 +1,26 @@
 module.exports = (client, message) => {
-    if (message.author.bot) return;
+
+    var prefix = client.config.prefix
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    //checks if the message didn't start with prefix 
   
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
-  
-    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
   
-    const cmd = client.commands.get(command);
+    const cmd = client.commands.get(command) || client.commands.find(al => al.aliases && al.aliases.includes(command));
+    // checks if the command has a aliases 
   
     if (!cmd) return;
-  
+
+    if (message.channel.type === 'dm') return;
+    // checks if the message was from a dm so he return cuz this will make the bot crash every time you send a message in dm
+        // or you can use if (!message.guild) return; so he checks if the message was not from a guild
+    try {
     cmd.run(client, message, args);
+    } catch (error) {
+    message.reply("there was an error with the command")
+    console.log(error)
+   }
+// checks if there was an error in the command 
 };
