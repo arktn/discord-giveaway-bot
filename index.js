9fb4751fb1022d5331edbb45790eedac70bfb6dc
jac <48368615+JacProsser@users.noreply.github.com>
2021-05-25 21:40:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ client.giveawaysManager = new GiveawaysManager(client, {
     }
 });
 
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
+    console.log(`Giveaway #${giveaway.messageID} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
+});
+
 ["aliases", "commands"].forEach(x => client[x] = new Collection());
 ["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
 
