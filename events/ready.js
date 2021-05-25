3b2b3e5273cbbf548eac2d9ac158e9e5b748c14a
jac <48368615+JacProsser@users.noreply.github.com>
2021-05-25 20:13:06 +0100

    Delete events directory

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client) => {
-    console.log(`Bot: ${client.user.tag}\nChannels: ${client.channels.cache.size}\nServers: ${client.guilds.cache.size}\nUsers: ${client.users.cache.size}`);
-    client.user.setActivity(client.config.prefix + "start | " + client.config.prefix + "help", { type: "PLAYING" });
-};
