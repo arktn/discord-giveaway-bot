37c0fdab17c75a36f341381189571eb9756cce75
jac <48368615+JacProsser@users.noreply.github.com>
2021-05-29 00:09:36 +0100

    Add new options in config.json (#72)
    * Add new features
    
    * update gif and json table
    
    * update adding ID

--- commands/Giveaways/start.js
+++ commands/Giveaways/start.js
@@ -11,6 +11,7 @@ module.exports = {
         aliases: [], // To add custom aliases just type ["alias1", "alias2"].
     },
     run: async (client, message, args) => {
+
         if (!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")) {
             return message.channel.send(':boom: You need to have the \`MANAGE_MESSAGES\` permissions to start giveaways.');
         }
@@ -34,34 +35,120 @@ module.exports = {
         if (!giveawayPrize) {
             return message.channel.send(':boom: Oh, it seems like you didn\'t give me a valid prize!');
         }
+        if (!config["Giveaway_Options"].showMention && config["Giveaway_Options"].giveawayRole) {
 
-        client.giveawaysManager.start(giveawayChannel, {
-            time: ms(giveawayDuration),
-            prize: giveawayPrize,
-            winnerCount: parseInt(giveawayNumberWinners),
-            hostedBy: config["Giveaway_Options"].hostedBy ? message.author : null,
-            messages: {
-                giveaway: (config["Giveaway_Options"].everyoneMention ? "@everyone\n\n" : "") + ":tada: **GIVEAWAY** :tada:",
-                giveawayEnded: (config["Giveaway_Options"].everyoneMention ? "@everyone\n\n" : "") + ":tada: **GIVEAWAY ENDED** :tada:",
-                timeRemaining: "Time remaining: **{duration}**!",
-                inviteToParticipate: "React with 🎉 to participate!",
-                winMessage: "Congratulations, {winners}! You won the **{prize}**!",
-                embedFooter: "Giveaways",
-                noWinner: "Not enough entrants to determine a winner!",
-                hostedBy: "Hosted by: {user}",
-                winners: "winner(s)",
-                endedAt: "Ended at",
-                units: {
-                    seconds: "seconds",
-                    minutes: "minutes",
-                    hours: "hours",
-                    days: "days",
-                    pluralS: false
+            giveawayChannel.send(`<@&${config["Giveaway_Options"].giveawayRole}>`).then((msg) => msg.delete({ timeout: 1000 }))
+            client.giveawaysManager.start(giveawayChannel, {
+                time: ms(giveawayDuration),
+                prize: giveawayPrize,
+                winnerCount: parseInt(giveawayNumberWinners),
+                hostedBy: config["Giveaway_Options"].hostedBy ? message.author : null,
+                messages: {
+                    giveaway: ":tada: **GIVEAWAY** :tada:",
+                    giveawayEnded: ":tada: **GIVEAWAY ENDED** :tada:",
+                    timeRemaining: "Time remaining: **{duration}**!",
+                    inviteToParticipate: "React with 🎉 to participate!",
+                    winMessage: "Congratulations, {winners}! You won the **{prize}**!",
+                    embedFooter: "Giveaways",
+                    noWinner: "Not enough entrants to determine a winner!",
+                    hostedBy: "Hosted by: {user}",
+                    winners: "winner(s)",
+                    endedAt: "Ended at",
+                    units: {
+                        seconds: "seconds",
+                        minutes: "minutes",
+                        hours: "hours",
+                        days: "days",
+                        pluralS: false
+                    }
                 }
-            }
-        });
+            });
+
+        } else if (config["Giveaway_Options"].showMention && config["Giveaway_Options"].giveawayRole) {
+
+            client.giveawaysManager.start(giveawayChannel, {
+                time: ms(giveawayDuration),
+                prize: giveawayPrize,
+                winnerCount: parseInt(giveawayNumberWinners),
+                hostedBy: config["Giveaway_Options"].hostedBy ? message.author : null,
+                messages: {
+                    giveaway: (config["Giveaway_Options"].showMention ? `<@&${config["Giveaway_Options"].giveawayRole}>\n\n` : "") + ":tada: **GIVEAWAY** :tada:",
+                    giveawayEnded: (config["Giveaway_Options"].showMention ? `<@&${config["Giveaway_Options"].giveawayRole}>\n\n` : "") + ":tada: **GIVEAWAY ENDED** :tada:",
+                    timeRemaining: "Time remaining: **{duration}**!",
+                    inviteToParticipate: "React with 🎉 to participate!",
+                    winMessage: "Congratulations, {winners}! You won the **{prize}**!",
+                    embedFooter: "Giveaways",
+                    noWinner: "Not enough entrants to determine a winner!",
+                    hostedBy: "Hosted by: {user}",
+                    winners: "winner(s)",
+                    endedAt: "Ended at",
+                    units: {
+                        seconds: "seconds",
+                        minutes: "minutes",
+                        hours: "hours",
+                        days: "days",
+                        pluralS: false
+                    }
+                }
+            });
+
+        } else if (!config["Giveaway_Options"].showMention && !config["Giveaway_Options"].giveawayRole) {
+            giveawayChannel.send(`@everyone`).then((msg) => msg.delete({ timeout: 1000 }))
+            client.giveawaysManager.start(giveawayChannel, {
+                time: ms(giveawayDuration),
+                prize: giveawayPrize,
+                winnerCount: parseInt(giveawayNumberWinners),
+                hostedBy: config["Giveaway_Options"].hostedBy ? message.author : null,
+                messages: {
+                    giveaway: ":tada: **GIVEAWAY** :tada:",
+                    giveawayEnded: ":tada: **GIVEAWAY ENDED** :tada:",
+                    timeRemaining: "Time remaining: **{duration}**!",
+                    inviteToParticipate: "React with 🎉 to participate!",
+                    winMessage: "Congratulations, {winners}! You won the **{prize}**!",
+                    embedFooter: "Giveaways",
+                    noWinner: "Not enough entrants to determine a winner!",
+                    hostedBy: "Hosted by: {user}",
+                    winners: "winner(s)",
+                    endedAt: "Ended at",
+                    units: {
+                        seconds: "seconds",
+                        minutes: "minutes",
+                        hours: "hours",
+                        days: "days",
+                        pluralS: false
+                    }
+                }
+            });
+
+        } else if (config["Giveaway_Options"].showMention && !config["Giveaway_Options"].giveawayRole) {
+            client.giveawaysManager.start(giveawayChannel, {
+                time: ms(giveawayDuration),
+                prize: giveawayPrize,
+                winnerCount: parseInt(giveawayNumberWinners),
+                hostedBy: config["Giveaway_Options"].hostedBy ? message.author : null,
+                messages: {
+                    giveaway: (config["Giveaway_Options"].showMention ? `@everyone\n\n` : "") + ":tada: **GIVEAWAY** :tada:",
+                    giveawayEnded: (config["Giveaway_Options"].showMention ? `@everyone\n\n` : "") + ":tada: **GIVEAWAY ENDED** :tada:",
+                    timeRemaining: "Time remaining: **{duration}**!",
+                    inviteToParticipate: "React with 🎉 to participate!",
+                    winMessage: "Congratulations, {winners}! You won the **{prize}**!",
+                    embedFooter: "Giveaways",
+                    noWinner: "Not enough entrants to determine a winner!",
+                    hostedBy: "Hosted by: {user}",
+                    winners: "winner(s)",
+                    endedAt: "Ended at",
+                    units: {
+                        seconds: "seconds",
+                        minutes: "minutes",
+                        hours: "hours",
+                        days: "days",
+                        pluralS: false
+                    }
+                }
+            });
+        }
+
 
         message.channel.send(`:tada: Done! The giveaway for the \`${giveawayPrize}\` is starting in ${giveawayChannel}!`);
-    },
+    }
 }
-
