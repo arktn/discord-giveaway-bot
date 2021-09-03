71b65a36fa06a3384a23b66f9f063235d63c4da9
Talal Malkawi <malkawitalal@gmail.com>
2021-09-03 22:46:20 +0200

    help.js edited

--- commands/Main/help.js
+++ commands/Main/help.js
@@ -1,6 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const { Bot_Info: { prefix } } = require('../../config.json')
-
+const config = require("../../config.json")
 module.exports = {
   config: {
     name: "help",
@@ -11,75 +10,59 @@ module.exports = {
     aliases: [], // To add custom aliases just type ["alias1", "alias2"].
   },
   run: async (client, message, args) => {
-    const { commands } = message.client;
+    let avatarOptions = {
+      format: 'png',
+      dynamic: true,
+      size: 1024
+    }
 
-      message.channel.send('Check your DMs!').then((msg) => msg.delete());
-      const options = {
-        format: 'png', 
-        dynamic: true, 
-        size: 1024
-      };
-      const LINK = 'https://github.com/ghaku/discord-giveaway-bot';
-      const BLURPLE = '7289da';
+    const embed = new MessageEmbed()
+      .setAuthor(
+        client.user.username,
+        client.user.displayAvatarURL({ ...avatarOptions }),
+        'https://github.com/ghaku/discord-giveaway-bot'
+      )
+      .setThumbnail(client.user.displayAvatarURL({ ...avatarOptions }))
+      .setTitle('Help')
+      .setURL('https://github.com/ghaku/discord-giveaway-bot')
+      .setColor('7289da')
+      .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/ghaku/discord-giveaway-bot)`)
+      .addFields({
+        name: `🎉 ${config["Bot_Info"].prefix}start [channel] [duration] [winners] [prize]`,
+        value: [
+          'The channel has to be visible to the giveaway bot.',
+          'Duration is stated in a number and a time variable.',
+          'Winners is stated in a positive number.',
+          'Prize can be anything except blank.'
+        ].join('\n')
+      }, {
+        name: '👥 Example:',
+        value: [
+          `⌨️ ${config["Bot_Info"].prefix}start #general 10m 1 $9.99 Nitro`,
+          `➡️ Creates a \`10 minute\` long giveaway with \`1\` winner and`,
+          `\`$9.99 Nitro\` as a prize in \`#general\`.`
+        ].join('\n')
+      }, {
+        name: `❌ ${config["Bot_Info"].prefix}end [message-id]`,
+        value: 'Message-ID has to be the **ID** of the giveaway message.\n**Not the link!**'
+      }, {
+        name: '👥 Example:',
+        value: `⌨️ ${config["Bot_Info"].prefix}end 892678258946659587\n➡️ Ends the giveaway with the message-ID \`892678258946659587\`.`
+      }, {
+        name: `🔍 ${config["Bot_Info"].prefix}reroll [message-id]`,
+        value: 'Message-ID has to be the **ID** of the giveaway message.\n**Not the link!**'
+      }, {
+        name: '👥 Example:',
+        value: `⌨️ ${config["Bot_Info"].prefix}reroll 892678258946659587\n➡️ Selects new winners for the giveaway with the message-ID \`892678258946659587\`.`
+      })
+      .setFooter('Made with 💖 and discord.js by ghaku', client.user.displayAvatarURL({ ...avatarOptions }))
 
-      const startCommand = commands.get('start');
-      const endCommand = commands.get('end');
-      const rerollCommand = commands.get('reroll');
-
-      let embed = new MessageEmbed()
-        .setAuthor(
-          client.user.username, 
-          client.user.displayAvatarURL({ ...options }), 
-          LINK
-        )
-        .setThumbnail(client.user.displayAvatarURL({ ...options }))
-        .setTitle('Help')
-        .setURL(LINK)
-        .setColor(BLURPLE)
-        .setDescription([
-          'You want to create a giveaway bot yourself?',
-          `Check out our Github page!](${LINK})`
-        ].join('\n'))
-        .addField(
-          `🎉 ${prefix}${startCommand.config.name} ${startCommand.config.usage}`,
-          [
-            'The channel has to be visible to the giveaway bot',
-            'Duration is stated in a number and a time variable.',
-            'Winners is stated in a positive number.',
-            'Prize can be anything except blank.'
-          ].join('\n'))
-        .addField(
-          '👥 Example:',
-          [
-            `⌨️ ${prefix}${startCommand.config.name} #general 10m 1 $9.99 Nitro`,
-            '➡️ Creates a \`10 minute\` long giveaway with \`1\` winner and',
-            '`$9.99 Nitro\` as a prize in \`#general\`.'
-          ].join('\n'))
-        .addField(
-          `❌ ${prefix}${endCommand.config.name} `,
-          [
-            'Message-ID has to be the **ID** of the giveaway message.',
-            '**Not the link!**'
-          ].join('\n'))
-        .addField(
-          '👥 Example:',
-        [
-          `⌨️ ${prefix}end 892678258946659587`,
-          `➡️ Ends the giveaway with the message-ID \`892678258946659587\`.`
-        ].join('\n'))
-        .addField(
-          `🔍 ${prefix}${rerollCommand.config.name} `,
-        [
-          'Message-ID has to be the **ID** of the giveaway message',
-          '**Not the link!**'
-        ].join('\n'))
-        .addField(
-          '👥 Example:',
-          [
-            `⌨️ ${prefix}${rerollCommand.config.name} 892678258946659587`,
-            `➡️ Selects new winners for the giveaway with the message-ID \`892678258946659587\`.`
-          ].join('\n'))
-        .setFooter('Made with 💖 and discord.js by ghaku', client.user.displayAvatarURL({ ...options }))
+    if (message.guild) {
+      message.channel.send('Check your DMs!');
+      message.delete();
       message.author.send(embed);
+    } else {
+      message.author.send(embed)
+    }
   },
 };
